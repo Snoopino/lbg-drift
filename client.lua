1f67659f95c52df4bf85991ca7a34926da6925db
lambdagit101 <48295237+lambdagit101@users.noreply.github.com>
2022-02-09 22:43:31 +0200

    Add files via upload

---
+++ client.lua
@@ -0,0 +1,23 @@
+local globalDriftState = false
+
+function ShowNotification(text)
+    SetNotificationTextEntry("STRING")
+    AddTextComponentString(text)
+    DrawNotification(false, false)
+end
+
+RegisterCommand("toggledrift", function()
+    local playerPed = GetPlayerPed(-1)
+    local playerVehicle = GetVehiclePedIsIn(playerPed, false)
+    if playerVehicle ~= 0 and IsVehicleOnAllWheels(playerVehicle) and GetPedInVehicleSeat(playerVehicle, -1) == playerPed then
+        globalDriftState = not GetDriftTyresEnabled(playerVehicle)
+        SetDriftTyresEnabled(playerVehicle, globalDriftState)
+        SetReduceDriftVehicleSuspension(playerVehicle, globalDriftState)
+        if globalDriftState then
+            ShowNotification("~b~Drift is ~g~ON")
+        else
+            ShowNotification("~b~Drift is ~r~OFF")
+        end
+    end
+end)
+RegisterKeyMapping("toggledrift", "Toggle Drift", "keyboard", "lshift")
---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'bodacious'
+game 'gta5'
+
+author 'lambdaguy101'
+version '1.0.0'
+
+dependencies {
+    '/gameBuild:2189',
+    '/native:0x5AC79C98C5C17F05'
+}
+
+client_script 'client.lua'
